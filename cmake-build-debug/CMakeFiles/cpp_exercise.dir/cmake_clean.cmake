e40a9b0a52bb5fc93d5cd22013c1bf9e6f7f9a28
Yueying <wangyy395@gmail.com>
2020-06-24 17:47:46 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/cpp_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_exercise.dir/test.cpp.o"
+  "cpp_exercise"
+  "cpp_exercise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by yueying wang on 6/24/20.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int wyy = 1000;
+    cout << "Hello wyy is:" << wyy << endl;
+    return 0;
+}
+
